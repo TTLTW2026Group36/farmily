2ee5757485bd2138c33d885f2b8935d6f705b3d8
LyPhat <23130229@st.hcmuaf.edu.vn>
2026-03-27 22:58:39 +0700

    fix: cập nhật lại Redirect URI và Client ID cho FB Login
    - sửa fb_redirect_uri (FamilyConstants) để trỏ về đúng context path
    - cập nhật lại client_id và redirect_uri (DangNhap.jsp) cho đồng bộ backend

--- src/main/java/group36/util/FarmilyConstants.java
+++ src/main/java/group36/util/FarmilyConstants.java
@@ -4,7 +4,7 @@ public class FarmilyConstants {
     // Configured based on user provided snippet
     public static final String FACEBOOK_CLIENT_ID = "962984642734580";
     public static final String FACEBOOK_CLIENT_SECRET = "dec807c6655c03fc81b3126a562beb37";
-    public static final String FACEBOOK_REDIRECT_URI = "http://localhost:8080/LoginFacebook/login";
+    public static final String FACEBOOK_REDIRECT_URI = "http://localhost:8080/farmily_v2_war_exploded/dang-nhap";
     public static final String FACEBOOK_LINK_GET_TOKEN = "https://graph.facebook.com/v19.0/oauth/access_token";
     public static final String FACEBOOK_LINK_GET_USER_INFO = "https://graph.facebook.com/me?fields=id,name,email,picture&access_token=";
 }
